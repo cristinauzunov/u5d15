65874c0dd1985e3062960d86d8932dfde3f3858b
cristinauzunov <cristina.uzunov99@gmail.com>
2026-07-20 11:17:04 +0200

    Initial commit

---
+++ src/test/java/cristinauzunov/u5d15/U5d15ApplicationTests.java
@@ -0,0 +1,13 @@
+package cristinauzunov.u5d15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
